869d83a61bfebdb72482d93358cee2a92a30f769
onwuvic <onwuzorvictor@outlook.com>
2018-06-13 12:42:43 +0100

    Change from commonjs to es6 module using import

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
+import app from './app';
 
-const app = require('./app');
 const port = parseInt(process.env.PORT, 10) || 3000;
 
 const server = app.listen(port, function() {
