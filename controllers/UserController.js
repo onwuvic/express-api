4a9582f1c0cedd955d06a5ab39d82a8e51af8706
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-06-27 13:55:54 +0100

    #158408138 Build out REST Endpoint for Resources (CRUD) #1

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,27 +1,47 @@
 import { User } from './../models/User';
 
-export const create = (req, res) => {
-    User.create({
-        username: req.body.username,
-        password: req.body.password 
-    },
-    function(error, user) {
-        if(error) return res.status(500).send('There was a problem adding the information to the database');
-        res.status(200).send(user);
+// GET ALL USERS FROM THE DATABASE
+export const users = (req, res) => {
+    User.find({}, (error, users) => {
+        if (error) return res.status(500).send('There was a problem finding the users');
+        res.status(200).json(users);
     });
 };
 
-export const user = (req, res) => {
-    let user = {
-        name: 'victor onwuzor',
-        email: 'onwuzorvictor@outlook.com',
-        password: 'vicky4all'
-    };
-    let db = process.env.DATA_URL;
-    res.status(200).send({user, db});
-
+// CREATE A NEW USER
+export const create = (req, res) => {
+    const user = new User(req.body);
+    user.save()
+        .then(user => res.status(201).json(user))
+        .catch(error => res.status(500).send(error.message));
 };
 
+// GET A SINGLE USER FROM THE DATABASE
+export const show = (req, res) => {
+    User.findOne({_id: req.params.id}, (error, user) => {
+        if(error) return res.status(500).send('There was a problem finding the user.');
+        if(!user) return res.status(404).send('No user found');
+        res.status(200).json(user);
+    });
+};
+
+// UPDATE A SINGLE USER FROM THE DATABASE
+export const update = (req, res) => {
+    User.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true, runValidators: true}, (error, user) => {
+        if (error) return res.status(500).send('There was a problem updating the user.');
+        res.status(200).json(user);
+    });
+};
+
+// DELETE A SINGLE USER FROM THE DATABASE
+export const destroy = (req, res) => {
+    User.findByIdAndRemove({_id: req.params.id}, (error, user) => {
+        if(error) return res.status(500).send('There was a problem deleting the user.');
+        res.status(200).send(`User ${user.username} was deleted`);
+    });
+};
+
+// HOME PAGE
 export const index = (req, res) => {
     res.status(200).send('Home Page');
 };
